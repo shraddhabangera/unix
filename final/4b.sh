3e8b001a0372f06b84c1fc2363f897877e0d9c9e
shraddha1410 <100298384+shraddhabangera@users.noreply.github.com>
2022-05-17 20:24:32 +0530

    Program

--- final/4b.sh
+++ final/4b.sh
@@ -1,20 +1,20 @@
 if [ $# -ge 1 ]
 then	
-	if [ $# -eq 2 ]
-	then
-		chd=$2
-	else
-		chd=.
-	fi
-	file=`find $chd -iname $1`
+  if [ $# -eq 2 ]
+   then
+	chd=$2
+    else
+	chd=.
+   fi
+     file=`find $chd -iname $1`
 	if [ `echo $file | wc -c` -gt 1 ]
 	then 
-		number=`ls -inum $file | grep -0 ^[0-9]*`
-		cd $chd
-		find . -inum $number
+	  number=`ls -inum $file | grep -0 ^[0-9]*`
+	  cd $chd
+	  find . -inum $number
 	else
-		echo No such file
+	  echo No such file
 	fi
 else
-	echo Provide filename
+    echo Provide filename
 fi
