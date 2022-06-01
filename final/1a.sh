e8bec44fcee4cc78c3493d93833651a06373e9ff
shraddha1410 <100298384+shraddhabangera@users.noreply.github.com>
2022-06-01 12:28:04 +0530

    Update 1a.sh

--- final/1a.sh
+++ final/1a.sh
@@ -2,7 +2,7 @@ if [ $# -eq 1 ]
 then
  if [ -d $1 ]
  then
- set -- `ls -Rl | grep "^-" | tr -s " " | cut -d " " -f 5,9- | sort -n | tail -1`
+ set -- `ls -Rl $1| grep "^-" | tr -s " " | cut -d " " -f 5,9- | sort -n | tail -1`
  echo "File name is:$2"
  echo "File size is:$1"
  else
